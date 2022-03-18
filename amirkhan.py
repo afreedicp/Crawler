0b2b33ddd1febff4a561730a8d118c395f34f5ba
afreedicp <afreedi@hamon.in>
2022-03-18 15:28:02 +0530

    function for geting downloadable image file set

--- amirkhan.py
+++ amirkhan.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 header = {
     'User-Agent': 'Mozilla/5.0 (MacintoshIntel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
@@ -13,23 +14,26 @@ def get_amirkhan(site_url):
         "div", {"class": "GalleryItems-module__searchContent___DbMmK"})
     link = gallery.find_all("a")
     for i in link:
-        ret.append((i['href']))
+        ret.append((i.text, i['href']))
     return ret
 
-    # image_list = soup.find(
-    #     "div", {"class": "GalleryItems-module__searchContent___DbMmK"})
-    # print(image_list)
-    # for i in gallery_list:
-    #     ret.append((i['src']))
-    # return ret
+
+def download_amirkhan(site_url):
+    r = requests.get(url=site_url, headers=header)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    gallery = soup.find(
+        "img", {"class": "AssetCard-module__image___dams4"})
+    thisimage = gallery['src']
+    print(thisimage)
+    return thisimage
 
 
 def main():
-    amir = get_amirkhan("https://www.gettyimages.in/photos/aamir-khan-actor")
+    amirlink = get_amirkhan(
+        "https://www.gettyimages.in/photos/aamir-khan-actor")
     head = "https://www.gettyimages.in"
-    for i in amir:
-        print("---------------", head+i,
-              "----------------")
+    for name, link in amirlink:
+        download_amirkhan(head+link)
 
 
 if __name__ == "__main__":
